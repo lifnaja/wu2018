0d12a269b1284515a2606d58447ead1060aa5b42
lifnaja <alif23669@gmail.com>
2018-02-04 09:46:04 +0700

    test 10 case

--- tests/AITest.php
+++ tests/AITest.php
@@ -17,4 +17,60 @@ final class AITest extends TestCase
         $expected_result = 'Female';
         $this->assertEquals($expected_result, $result);
     }
+
+    public function  testGender_MaleCase2()
+    {
+        $result = AI::getGender('สวัสดีครัช');
+        $expected_result = 'Male';
+        $this->assertEquals($expected_result, $result);
+    }
+
+    public function  testSentiment_Positive()
+    {
+        $result = AI::getSentiment('ดีใจ');
+        $expected_result = 'Positive';
+        $this->assertEquals($expected_result, $result);
+    }
+
+    public function  testSentiment_Neutral()
+    {
+        $result = AI::getSentiment('เฉยๆ');
+        $expected_result = 'Neutral';
+        $this->assertEquals($expected_result, $result);
+    }
+
+    public function  testSentiment_Negative()
+    {
+        $result = AI::getSentiment('ไรวะ');
+        $expected_result = 'Negative';
+        $this->assertEquals($expected_result, $result);
+    }
+
+    public function  testLanguages_Thai()
+    {
+        $result = AI::getLanguages('ไรวะ');
+        $expected_result = ['TH'];
+        $this->assertEquals($expected_result, $result);
+    }
+
+    public function  testLanguages_English()
+    {
+        $result = AI::getLanguages('hello');
+        $expected_result = ['EN'];
+        $this->assertEquals($expected_result, $result);
+    }
+
+    public function  testLanguages_ThaiEnlish()
+    {
+        $result = AI::getLanguages('ไรวะqweqwe');
+        $expected_result =  ['TH', 'EN'];
+        $this->assertEquals($expected_result, $result);
+    }
+
+    public function  testRudeWords_notRude()
+    {
+        $result = AI::getRudeWords('อะไรเอ๋ย');
+        $expected_result =  ['พูดเพราะมาก'];
+        $this->assertEquals($expected_result, $result);
+    }
 }
